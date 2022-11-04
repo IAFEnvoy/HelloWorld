f4a85bd736631052d8bf2c6eacf99684b183c01d
IAFEnvoy <tanksheng545@126.com>
2022-11-04 12:36:09 +0800

    2

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System.Windows.Forms;
+
+namespace HelloWorld{
+    internal static class Program{
+        [STAThread]
+        static void Main(string[] args){
+           MessageBox.Show("Hello world");
+        }
+    }
+}
